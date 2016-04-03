5ab5d3a3db1af8589c5e793b0e54c3df39386225
d0f <284178359@qq.com>
2016-04-03 23:13:19 +0800

    add user to kelvim

--- general-lisp/appearance.el
+++ general-lisp/appearance.el
@@ -21,7 +21,7 @@
 (global-hl-line-mode 1)
 (set-face-background 'region "#202020")
 ;; No scratch Message
-(setq initial-scratch-message ";; So this is you again, Kelvin\n\n")
+(setq initial-scratch-message ";; So this is you again, kelvin\n\n")
 
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
